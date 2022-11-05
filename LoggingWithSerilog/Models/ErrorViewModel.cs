97c8ad01bf567a9e62bfe956173966ad9f674de5
Thommelutten <thomas.m.elbo@gmail.com>
2022-11-05 13:17:11 +0100

    Initial commit

---
+++ LoggingWithSerilog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoggingWithSerilog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
